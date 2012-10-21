f8aab6a9da619df5cbd22b6e2977dd74d4c2567f
Nishanth Amuluru <nishanth.amuluru@gmail.com>
2012-10-21 10:28:00 +0530

    Added a method to calculate a range of points between two points

--- utils.py
+++ utils.py
@@ -3,6 +3,48 @@
 from urllib2 import urlopen
 from json import loads
 
+import math
+
+import numpy as np
+
+def gc_dist(point1, point2):
+    """ Calculates the great-circle distance between two points on earth.
+
+    >>> point1 = (13.060422, 80.249583)
+    >>> point2 = (12.9165167, 79.1324986)
+
+    >>> gc_dist(point1, point2)
+    122.089163396
+    """
+
+    radius = 6371
+    lat1, lng1 = point1
+    lat2, lng2 = point2
+
+    lat1 = lat1 * math.pi / 180
+    lng1 = lng1 * math.pi / 180
+    lat2 = lat2 * math.pi / 180
+    lng2 = lng2 * math.pi / 180
+
+    return radius * math.acos(math.sin(lat1) * math.sin(lat2) + 
+                              math.cos(lat1) * math.cos(lat2) * math.cos(lng2 - lng1)
+                             )
+
+
+def range_of_points(point1, point2, nPoints):
+    """ An extended version of the ``linspace`` method in ``numpy``.
+    Generates a range of points between two given points.
+    """
+
+    x1, y1 = point1
+    x2, y2 = point2
+
+    x_range = np.linspace(x1, x2, nPoints)
+    y_range = np.linspace(y1, y2, nPoints)
+
+    return zip(x_range, y_range)
+
+
 def elevation(point):
     """ Returns the elevation (in mtrs) of the given point.
     Ex:
